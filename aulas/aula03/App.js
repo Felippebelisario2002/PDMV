db6a8bcd137ecaa5ee35284cc7b6e5a63d782649
Felippebelisario2002 <felippefonsecabelisario@gmail.com>
2025-03-06 21:54:17 -0300

    Adicionando arquivos da pratica02

---
+++ aulas/aula03/App.js
@@ -0,0 +1,28 @@
+import { View } from "react-native";
+
+function App() {
+  return (
+    <View 
+      style={{ flex: 1, flexDirection: "row", backgroundColor: "white" }}>
+      <View 
+        style={{ 
+          flex: 1, 
+          justifyContent:"space-evenly", 
+          alignItems:"strech", 
+          backgroundColor: "white", 
+          }}
+       >
+        <View
+          style={{ height: 50, width: 50, backgroundColor: "black" }}
+        ></View>
+        <View style={{ height: 50, width: 50, backgroundColor: "blue" }}></View>
+        <View
+          style={{ height: 50, width: 50, backgroundColor: "yellow" }}
+        ></View>
+      </View>
+      <View style={{ flex: 1, backgroundColor: "green" }}></View>
+    </View>
+  );
+}
+
+export default App;

890cd908d1ac6f154bcd8a48a2723edd5b3c4ed1
Felippebelisario2002 <felippefonsecabelisario@gmail.com>
2025-03-13 21:53:22 -0300

    atualizando o git pratica3

---
+++ praticas/pratica03/App.js
@@ -0,0 +1,12 @@
+import ListScreen from "./screens/ListScreen";
+import GalleryScreen from "./screens/GalleryScreen.js"
+import HomeScreen from "./screens/HomeScreen";
+
+
+
+function App() {
+  return <HomeScreen />;
+}
+
+
+export default App;

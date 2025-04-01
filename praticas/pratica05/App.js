1d2fa0ce6d33815451b11481325bc950b1dcb042
Felippebelisario2002 <felippefonsecabelisario@gmail.com>
2025-04-01 20:02:45 -0300

    Adicionando arquivos da pratica05

---
+++ praticas/pratica05/App.js
@@ -0,0 +1,14 @@
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import LoginScreen from "./screens/LoginScreen";
+import ChatScreen from "./screens/ChatScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+
+
+function App() {
+  return <SettingsScreen>
+
+  </SettingsScreen>;
+}
+
+export default App;
